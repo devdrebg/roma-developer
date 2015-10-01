88528e3c9ba07a82dcf822dc27d3259fab7e659c
unknown <andreromario@live.com>
2015-10-01 10:52:32 -0300

    Subindo comentários de documentação

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+		</main>
+
+		<footer>
+			
+		</footer>
+		<script src="<?php echo BASEPATH; ?>js/all.min.js"></script>
+		<!-- Codificado por Roma Developer / http://www.andreromario.com.br/ -->
+	</body>
+</html>
